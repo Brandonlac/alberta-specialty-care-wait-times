2385aa99a1f69168d39dbe20c53ada052c953444
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2024-03-02 10:24:25 -0700

    uses json for procedures

--- src/components/Procedure.js
+++ src/components/Procedure.js
@@ -1,4 +1,3 @@
-// Procedure.js
 import React, { useState } from "react";
 import {
     View,
@@ -6,231 +5,123 @@ import {
     TouchableOpacity,
     StyleSheet,
     ScrollView,
+    TouchableWithoutFeedback,
 } from "react-native";
 
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faCircle } from "@fortawesome/free-regular-svg-icons";
 import {
+    faCheckCircle,
     faCaretDown,
     faPersonWalking,
-    faCheckCircle,
 } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import procedureData from "./../procedureData.json";
 
 const Procedure = ({ navigation }) => {
     const [showDropdown, setShowDropdown] = useState(false);
-    const [selectedValue, setSelectedValue] = useState(null);
-    const [data, setData] = useState(null);
+    const [selectedProcedure, setSelectedProcedure] = useState(null);
     const [selectedCircle, setSelectedCircle] = useState(null);
+    const [selectedCircleData, setSelectedCircleData] = useState(null);
 
-    const upperHead = [
-        { label: "Cataract Surgery 1st Eye Only", value: "cataract" },
-        { label: "Interventions on the Brain and Spinal Cord", value: "brain" },
-        { label: "Interventions on the Eyelid", value: "eyelid" },
-        { label: "Other Interventions on the Eye", value: "eye" },
-        { label: "Other Interventions on the Eye", value: "eye" },
-    ];
-
-    const lowerHead = [
-        {
-            label: "Interventions on the Nose and Nasal Cartilage",
-            value: "nose",
-        },
-        { label: "Other Interventions on the Ear", value: "ear" },
-        {
-            label: "Other Interventions on the Mouth and Throat",
-            value: "throat",
-        },
-        { label: "Other: Head, Nasal Cavity and Sinuses", value: "sinus" },
-        { label: "Tonsillectomy", value: "tonsil" },
-        { label: "Tubes in the Ears", value: "tubes" },
-    ];
-
-    const shoulder = [
-        {
-            label: "Interventions on the Shoulder",
-            value: "shoulder",
-        },
-    ];
-
-    const heart = [
-        { label: "Coronary Artery Bypass Graft (CABG)", value: "cabg" },
-        { label: "Heart Valve Surgery", value: "valve" },
-        {
-            label: "Implantation of Pacemaker and Other Devices",
-            value: "pacemaker",
-        },
-        {
-            label: "Interventions on the Primary Blood Vessels",
-            value: "primaryvessels",
-        },
-        { label: "Other: Heart and Related Structures", value: "structure" },
-        { label: "Other: Upper Body Blood Vessels", value: "vessels" },
-    ];
-
-    const chest = [
-        {
-            label: "Interventions on the Respiratory System",
-            value: "respiratory",
-        },
-        { label: "Mastectomy: Removal of Breast", value: "mastectomy" },
-        { label: "Other Interventions on the Breast", value: "breast" },
-        {
-            label: "Spinal Vertebrae, Discs and Muscles of the Back",
-            value: "back",
-        },
-    ];
-
-    const elbow = [
-        { label: "Interventions on the Arm and Elbow", value: "Elbow" },
-    ];
-
-    const wrist = [
-        { label: "Carpal Tunnel Release", value: "Carpal" },
-        { label: "Interventions on the Hand and Wrist", value: "Wrist" },
-    ];
-
-    const stomach = [
-        { label: "Gall Bladder Removal", value: "Gall Bladder" },
-        { label: "Hernia Repair", value: "Hernia" },
-        { label: "Interventions on the Large Intestine", value: "Intestine" },
-        { label: "Interventions on the Small Intestine", value: "Intestine" },
-        { label: "Interventions on the Stomach", value: "Stomach" },
-        {
-            label: "Other Interventions on the Digestive System ",
-            value: "Digestive",
-        },
-        {
-            label: "Other: Liver, Spleen, Pancreas and Gall Bladder",
-            value: "Other",
-        },
-    ];
-
-    const hip = [
-        { label: "Hip Replacement Surgery", value: "Hip" },
-        { label: "Interventions on the Lower Leg", value: "Leg" },
-        { label: "Other Interventios on the Hip and Thigh", value: "Thigh" },
-        { label: "Varicose Vein(Leg) Surgery", value: "Leg" },
-    ];
-
-    const genitals = [
-        { label: "Hysterectomy", value: "Vagina" },
-        { label: "Interventions on the Urinary Bladder", value: "Bladder" },
-        { label: "Interventions on the Urinary System", value: "Bladder" },
-        { label: "Interventions on the Vagina", value: "Vagina" },
-        { label: "Muscles and Bones of the Trunk and Pelvis", value: "Pelvis" },
-        {
-            label: "Other Interventions on the Female Genital Tract",
-            value: "Genital",
-        },
-        {
-            label: "Other Interventions on the Male Genital System",
-            value: "Genital",
-        },
-        { label: "Other: Lower Body Blood Vessels", value: "Lower Body" },
-        { label: "Prostate Surgery", value: "Prostate" },
-        { label: "Tubal Ligation for Sterilization", value: "Tubal" },
-    ];
-
-    const knee = [
-        { label: "Knee Arthroscopy", value: "Knee" },
-        { label: "Knee Replacement Surgery", value: "Knee" },
-        { label: "Other Interventions on the Knee", value: "Knee" },
-    ];
-
-    const foot = [
-        { label: "Interventions on the Ankle and Foot", value: "Foot" },
-    ];
-
-    const other = [
-        { label: "Cancer Services", value: "cancer" },
-        { label: "CT Scans", value: "ct" },
-        { label: "Interventions on Lymph Nodes", value: "lymph" },
-        { label: "MRI Scans", value: "mri" },
-        { label: "Other Interventions on the Nerves", value: "nerves" },
-        { label: "Therapeutic Interventions on the Skin", value: "skin" },
+    const circlePositions = [
+        { x: 165, y: 190, data: procedureData["upperHead"] },
+        { x: 210, y: 220, data: procedureData["lowerHead"] },
+        { x: 150, y: 255, data: procedureData["shoulder"] },
+        { x: 190, y: 295, data: procedureData["heart"] },
+        { x: 160, y: 300, data: procedureData["chest"] },
+        { x: 220, y: 325, data: procedureData["elbow"] },
+        { x: 100, y: 315, data: procedureData["wrist"] },
+        { x: 165, y: 345, data: procedureData["stomach"] },
+        { x: 165, y: 365, data: procedureData["genitals"] },
+        { x: 135, y: 345, data: procedureData["hip"] },
+        { x: 200, y: 395, data: procedureData["knee"] },
+        { x: 220, y: 470, data: procedureData["foot"] },
+        { x: 300, y: 501, data: procedureData["other"] },
     ];
 
     const handleSelect = (value) => {
-        setSelectedValue(value);
+        setSelectedProcedure(value);
         setShowDropdown(false);
     };
 
-    const [circlePositions, setCirclePositions] = useState([
-        { x: 165, y: 190, data: upperHead },
-        { x: 210, y: 220, data: lowerHead },
-        { x: 150, y: 255, data: shoulder },
-        { x: 190, y: 295, data: heart },
-        { x: 160, y: 300, data: chest },
-        { x: 220, y: 325, data: elbow },
-        { x: 100, y: 315, data: wrist },
-        { x: 165, y: 345, data: stomach },
-        { x: 165, y: 365, data: genitals },
-        { x: 135, y: 345, data: hip },
-        { x: 200, y: 395, data: knee },
-        { x: 220, y: 470, data: foot },
-        { x: 300, y: 501, data: other },
-    ]);
-
     const handleCirclePress = (position, index) => {
         setSelectedCircle(index);
-        setData(position.data);
+        setSelectedCircleData(position.data);
+        setShowDropdown(true);
     };
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity
-                onPress={() => setShowDropdown(!showDropdown)}
-                style={styles.dropdownButton}
-            >
-                <Text style={styles.dropdownButtonText}>
-                    {selectedValue ? selectedValue.label : "Select a procedure"}
-                </Text>
-                <FontAwesomeIcon
-                    icon={faCaretDown}
-                    size={15}
-                    style={styles.caretIcon}
-                />
-            </TouchableOpacity>
-            {showDropdown && selectedCircle !== null && (
-                <ScrollView style={styles.dropdownList}>
-                    {data.map((item) => (
-                        <TouchableOpacity
-                            key={item.value}
-                            style={styles.dropdownItem}
-                            onPress={() => handleSelect(item)}
-                        >
-                            <Text>{item.label}</Text>
-                        </TouchableOpacity>
-                    ))}
-                </ScrollView>
-            )}
 
-            {circlePositions.map((position, index) => (
+    const closeDropdown = () => {
+        setShowDropdown(false);
+    };
+
+    return (
+        <TouchableWithoutFeedback onPress={closeDropdown}>
+            <View style={styles.container}>
                 <TouchableOpacity
-                    key={index}
-                    style={[
-                        styles.circle,
-                        selectedCircle === index,
-                        {
-                            left: position.x,
-                            top: position.y,
-                        },
-                    ]}
-                    onPress={() => handleCirclePress(position)}
+                    onPress={() => setShowDropdown(!showDropdown)}
+                    style={styles.dropdownButton}
                 >
+                    <Text style={styles.dropdownButtonText}>
+                        {selectedProcedure
+                            ? selectedProcedure
+                            : "Select a procedure"}
+                    </Text>
                     <FontAwesomeIcon
-                        icon={faCircle}
-                        size={30}
-                        color={"#d3d3d3"}
+                        icon={faCaretDown}
+                        size={15}
+                        style={styles.caretIcon}
                     />
                 </TouchableOpacity>
-            ))}
+                {showDropdown && selectedCircle !== null && (
+                    <ScrollView
+                        style={[styles.dropdownList, { maxHeight: "60%" }]}
+                    >
+                        {selectedCircleData &&
+                            selectedCircleData.map(
+                                (procedure, procedureIndex) => (
+                                    <TouchableOpacity
+                                        key={procedureIndex}
+                                        style={styles.dropdownItem}
+                                        onPress={() => handleSelect(procedure)}
+                                    >
+                                        <Text>{procedure}</Text>
+                                    </TouchableOpacity>
+                                )
+                            )}
+                    </ScrollView>
+                )}
 
-            <FontAwesomeIcon
-                icon={faPersonWalking}
-                style={styles.icon}
-                size={300}
-            />
-        </View>
+                {circlePositions.map((position, index) => (
+                    <TouchableOpacity
+                        key={index}
+                        style={[
+                            styles.circle,
+                            selectedCircle === index && styles.selectedCircle,
+                            { left: position.x, top: position.y },
+                        ]}
+                        onPress={() => handleCirclePress(position, index)}
+                    >
+                        <FontAwesomeIcon
+                            icon={
+                                selectedCircle === index
+                                    ? faCheckCircle
+                                    : faCircle
+                            }
+                            size={30}
+                            color={
+                                selectedCircle === index ? "#3BB143" : "#d3d3d3"
+                            }
+                        />
+                    </TouchableOpacity>
+                ))}
+
+                <FontAwesomeIcon
+                    icon={faPersonWalking}
+                    style={styles.icon}
+                    size={300}
+                />
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
